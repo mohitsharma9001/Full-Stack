0d3edff885c9a6f65f52d4d92dff96a4ce65406e
Mohit Sharma <mohitsharma90015656@gmail.com>
2022-12-15 16:31:38 +0530

    done

--- server-side/index.js
+++ server-side/index.js
@@ -3,18 +3,11 @@ import { connection } from "./database/db.js";
 import router from "./routes/Route.js"
 import cors from "cors";
 import bodyParser from "body-parser";
-
-
-
 const app = express();
 app.use(bodyParser.json({extended : true}))
 app.use(bodyParser.urlencoded({extended : true}))
 app.use(cors());
 app.use("/",router);
-
-
-
-
 
 
 const PORT = 8000;
